228728450f1b6ec18899b7661376fbdf137053c8
IanNpdx5 <163360899+IanNpdx5@users.noreply.github.com>
2024-04-08 11:44:31 -0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -1,5 +1,1 @@
-var clickerButton = document.getElementById("clicker");
-    var onButtonClick = function() {
-        clickerButton.textContent = "Oh wow, you clicked me!";
-    };
-    clickerButton.addEventListener("click", onButtonClick);
+//bye
